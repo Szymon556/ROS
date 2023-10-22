47e0b3522297117e88b583b7bcc98a8dcd887cd2
Szymon <szymon@Ubuntu20.04.myguest.virtualbox.org>
2023-10-22 11:27:26 +0200

    Dodanie sterowanie rpm w lewo i prawo

--- scripts/rpm_counter.py
+++ scripts/rpm_counter.py
@@ -4,10 +4,7 @@ import rospy
 from std_msgs.msg import UInt16
 from std_msgs.msg import Float32
 
-class rpm_counter:
-
-    def __init__(self):
-        '''
+'''
         W kodzie ponizej licze predkosc katowa na zasadzie: od aktualnej pozycji 
         odejmij poprzednia zeby zobaczysz ile "pozycji/pulsow" wykona enkoder
         w czasie 0.01s (czyli 100hz) nastepnie dziele ta liczbe przez 4096 zeby widziec ile
@@ -15,21 +12,30 @@ class rpm_counter:
         w minutach. Uwzglednilem tez przypadek gdy roznica miedzy poprzednia pozycja i aktualna praktycznie sie 
         nie zmienia (albo prawie sie nie zmienia) i nastepuje dzielenie przez 0(gdy cs < 13). Z doswiadczen zauwazylem ze <3 jest optymalna wartoscia.
         Rowniez uwzglednilem przypadek gdy wartosc np: prev = 4090 a data.data = 50 to wtedy zeby nie przekalamywac wyniku wyknuje 4096 - prev
-        zeby otrzymac prawidlowa odlegosc jaka pokonal silnik podczas 0.01s.
-        '''
+        zeby otrzymac prawidlowa odlegosc jaka pokonal silnik podczas 0.01s. Linijka abs(data.data - self.prev) > 4000 jest po ze w przypadku
+        gdy cs<13 wartosci na topicu get_position wynosze 0,1,4090 nie wiem dlaczego tak sie dzieje ale ta linijak zapobiega pojawieniu sie 
+        absurdalnych wartosci rpm gdy silnik stoji w miesjcu.
+'''
+
+class rpm_counter:
+
+    def __init__(self):
+       
         self.prev = 0
         self.sub = rospy.Subscriber("/virtual_dc_motor/get_position",UInt16,self.callback)
         self.pub = rospy.Publisher("/virtual_dc_motor_driver/get_velocity",Float32,queue_size=10)
 
     def callback(self,data):
+        
         if abs(data.data - self.prev) <3:
             self.rpm = 0
-        elif data.data < self.prev:
-            self.rpm = (((4096-self.prev) + data.data)/4096) * 600
+        elif abs(data.data - self.prev) > 4000:
+            self.rpm = 0
         else:
             self.rpm = (abs((data.data-self.prev))/4096) *600
         self.prev = data.data
-        rospy.loginfo(self.rpm)
+        rospy.loginfo(f"{self.rpm} {data.data}")
+        
         self.pub.publish(self.rpm)
     
    
--- scripts/velocity_controller.py
+++ scripts/velocity_controller.py
@@ -8,11 +8,9 @@ from std_msgs.msg import Float32
 '''
 Program ponizej pobiera docelowa predkosc w rpm oraz aktualna jezeli akutalna
 predkosc jest mniejsza od docelowej to zwieksza inkrementacyjnie cs o 1, w przeciwnym razie zmniejsza. 
-Tak wiem ze program dziala tylko przy jednym kierunku obrotu silnika.
-nie wiem jak mialy by wygladac implementacja kodu zeby sterowala ruchem silnika w przeciwnym kierunku, -10 obrotow na minute jako docelowa predkosc, podane
-na set_velocity_goal?
-W takim wypadku musialbym przerobic program rpm_counter zeby poradzil sobie z wyliczeniem predkosci gdy silnik kreci sie w przeciwna strone(bo wtedy data.data < prev)
-Moja propozycja na ulepszenie tego programu byloby pobieranie wartosci o aktualnym cs silnika i na podstawie tego nim sterowac.
+if (abs(self.goal_rpm) < abs(self.actual_rpm)) and abs(self.actual_rpm) < 37: ta linijka jest po to
+ze gdy ustawimy np rpm 200 co jest poza zakresem silnika (silnik bedzie wtedy utrzymywal maksymalne obroty) a nstepnie damy rpm -10 co juz jest w zakresie pracy silnika
+to prgram zeruje cs i ustawia zadana predkosc. 
 '''
 
 
@@ -20,41 +18,48 @@ class velocity_controll:
     
     
     def __init__(self) -> None:
-        self.actual_rpm = 0
-        self.goal_rpm = 0
-        self.cs = 0
-        self.prev_cs = 0
+        self.actual_rpm = 0 #aktualne rpm
+        self.goal_rpm = 0 #docelowe rpm
+        self.cs = 0 #ustawiam controll speed
+        self.prev_cs = 0 #zmienna gdy przekroczymy maksymalne rpm dla silnika to ustawiamy ostania poprawna wartosc zeby silnik pracowal w zakresie o -100 do `100`
         self.velocity_goal_subscriber = rospy.Subscriber("/virtual_dc_motor_controller/set_velocity_goal",Float32,callback=self.get_goal_rpm)
         self.velocity_subscriber = rospy.Subscriber("/virtual_dc_motor_driver/get_velocity",Float32,callback=self.calculate)
         self.pub_cs = rospy.Publisher("/virtual_dc_motor/set_cs",Int8,queue_size=10)
+
 #pobierz aktualna predkosc i wysteruj odpowiednio silnik
+
     def calculate(self,data):
+        
         self.actual_rpm = data.data
-        if self.cs <= 100 and self.cs >= 0:#zeby nie wyjsc po za zakres cs
-                
-            
+
+        if self.cs < 0 and self.actual_rpm > 0: #do obslugi "ujemnych rpm"
+
+                self.actual_rpm = self.actual_rpm * (-1)
+
+        elif self.cs > 0 and self.actual_rpm < 0:
+                self.actual_rpm = abs(self.actual_rpm)
+        if self.cs < 100 and self.cs > -100:    #zeby nie wyjsc po za zakres cs
+
             if self.actual_rpm < self.goal_rpm:
                 self.cs += 1
-                self.pub_cs.publish(self.cs)
             elif self.actual_rpm > self.goal_rpm:
-               
-            
                 self.cs = self.cs - 1
-                if self.cs < 0:
-                   
-                   self.cs = 0
-                self.pub_cs.publish(self.cs)
             else:
-                self.pub_cs.publish(self.cs)
-            self.prev_cs = self.cs
-        else:#jesli predkosc jes za duza ustaw ostania predkosc(bedzie tutaj maly poslizg i ustawia z reguly maksymalna predkosc)
-            self.cs = abs(self.prev_cs)
-            if self.goal_rpm < self.actual_rpm:#jesli zmienimy predkosc na mniejsza to sprubuje ja osiagnac.
-                self.cs = 0
-            self.pub_cs.publish(self.prev_cs)
-        #rospy.loginfo(f"self.cs = {self.cs} self.actual_rpm = {self.actual_rpm} self.goal_rpm = {self.goal_rpm} self.actual_rpm.data {data.data}")
+                self.cs = self.prev_cs
 
-    def get_goal_rpm(self,data):#pobierz docelowa predkosc
+            self.prev_cs = self.cs
+            self.pub_cs.publish(self.cs)
+
+        else:  
+            
+            if (abs(self.goal_rpm) < abs(self.actual_rpm)) and abs(self.actual_rpm) < 37:   #jesli zmienimy predkosc na mniejsza to sprubuje ja osiagnac.
+                self.cs = 0
+            
+            self.pub_cs.publish(self.prev_cs)
+
+        rospy.loginfo(f"self.cs = {self.cs} self.actual_rpm = {self.actual_rpm} self.goal_rpm = {self.goal_rpm} self.actual_rpm.data {data.data}")
+        
+    def get_goal_rpm(self,data):    #pobierz docelowa predkosc
        
        self.goal_rpm = data.data
 
@@ -67,6 +72,7 @@ class velocity_controll:
     
 
 if __name__ == "__main__":
+
     rospy.init_node("rpm_counter",anonymous=True)
     velocity_controll()
     rospy.spin()
